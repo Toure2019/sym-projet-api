313a6cc0a5bb458614b4345e02ea5e57ad5357b5
Toure2019 <51404116+Toure2019@users.noreply.github.com>
2019-12-26 11:22:39 +0100

    second commit & end tuto: api region et departement

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,6 +21,32 @@ class ApiController extends AbstractController
         // dump($mesRegionsObj); die();
         return $this->render('api/index.html.twig', [
             'mesRegions' => $mesRegions
+        ]);
+    }
+
+    /**
+     * @Route("/listeDepsParRegion", name="listeDepsParRegion")
+     */
+    public function listeDepsParRegion(Request $request, SerializerInterface $serializer)
+    {
+        // Je recupère la région sélectionnée ds le formulaire
+        $codeRegion = $request->query->get('region'); 
+        //Je recupère les regions
+        $mesRegions = file_get_contents('https://geo.api.gouv.fr/regions');
+        $mesRegions = $serializer->deserialize($mesRegions, 'App\Entity\Region[]', 'json');
+
+        // Récupération des départements
+        if($codeRegion == null || $codeRegion == 'Toutes') {
+            $mesDeps = file_get_contents('https://geo.api.gouv.fr/departements');
+        } else {
+            $mesDeps = file_get_contents('https://geo.api.gouv.fr/regions/'.$codeRegion.'/departements');
+        }
+        // Décodage du format json en tableau
+        $mesDeps = $serializer->decode($mesDeps, 'json');
+
+        return $this->render('api/listDepsParRegion.html.twig', [
+            'mesRegions' => $mesRegions,
+            'mesDeps' => $mesDeps
         ]);
     }
 }
